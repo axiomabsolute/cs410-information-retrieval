3c73038482b068a872a8fbf1fef5aaaa44a83e6e
Devon Terrell <axiomabsolute@gmail.com>
2017-10-19 15:01:58 -0400

    Adjust example to allow for not reloading data, show lookup example for SqlIRSystem

--- example.py
+++ example.py
@@ -44,6 +44,30 @@ print_timing("Building IR system")
 # table_rows.sort(key=lambda x: (x[0], x[2], -1*x[4]))
 # print(tabulate(table_rows, headers=table_headers))
 
-sqlsystem = SqlIRSystem('example.db.sqlite', index_methods, scorer_methods, piece_paths)
+# sqlsystem = SqlIRSystem('example.db.sqlite', index_methods, scorer_methods, piece_paths)
+
+sqlsystem = SqlIRSystem('example.db.sqlite', index_methods, scorer_methods, piece_paths, False)
+print_timing("Sampling snippets for demonstration")
+sample_pieces = (corpus.parse(piece) for piece in random.sample(piece_paths, min(5, len(piece_paths))))
+random_snippets = random.sample(list(get_snippets_for_pieces(sample_pieces)), 10)
+scores = [sqlsystem.lookup(s) for s in random_snippets]
+scored_snippets = zip(random_snippets, scores)
+
+print_timing("Printing results")
+print("==========================================================================")
+table_headers = ['Query Source', 'Query Line', 'Grading Method', 'Piece', 'Grade']
+table_rows = []
+for (snippet, scores) in scored_snippets:
+    for (scorer, score) in scores.items():
+        for (matching_piece, grade) in score.items():
+            table_rows.append([
+                "%s %s (m %s)" % (snippet.piece, snippet.part, snippet.offset),
+                ' '.join(snippet.simple_line()),
+                scorer,
+                matching_piece,
+                grade
+            ])
+table_rows.sort(key=lambda x: (x[0], x[2], -1*x[4]))
+print(tabulate(table_rows, headers=table_headers))
 
 input("\nDone - press enter to exit.")
