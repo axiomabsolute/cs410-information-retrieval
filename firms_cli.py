3d41babf0d97b83c3dfedc2bbafb2998a5e7d217
Devon Terrell <zeugmasyllepsis@gmail.com>
2017-11-29 20:20:21 -0500

    Few more commands

--- firms_cli.py
+++ firms_cli.py
@@ -24,6 +24,7 @@ from firms.graders import bm25_factory
 from firms.stemmers import index_key_by_pitch, index_key_by_simple_pitch, index_key_by_interval,\
     index_key_by_contour, index_key_by_rythm, index_key_by_normalized_rythm
 
+
 index_methods = {
     'By Pitch': index_key_by_pitch,
     'By Simple Pitch': index_key_by_simple_pitch,
@@ -51,7 +52,7 @@ def connect(path):
 @click.command("add")
 @click.option('--piece', help="Path to MusicXML file")
 @click.option('--path', default=DEFAULT_DB_PATH, help="Path to sqlite DB file")
-def add_piece(piece_path, db_path=DEFAULT_DB_PATH):
+def add_piece(piece_path, db_path):
     """Add musicXML piece to firms index"""
     sqlIrSystem = connect(db_path)
     piece = corpus.parse(piece_path)
@@ -60,7 +61,7 @@ def add_piece(piece_path, db_path=DEFAULT_DB_PATH):
 @click.command("tiny")
 @click.option('--query', help="Snippet of target piece, in TinyNotation")
 @click.option('--path', default=DEFAULT_DB_PATH, help="Path to sqlite DB file")
-def query_tiny(query, path=DEFAULT_DB_PATH):
+def query_tiny(query, path):
     """
         Query for piece using tiny notation
 
@@ -71,6 +72,37 @@ def query_tiny(query, path=DEFAULT_DB_PATH):
     notes = stream.recurse().notesAndRests
     results = sqlIrSystem.query(notes)
     return print_results(results)
+
+@click.group()
+def info():
+    """
+    Show information on one or more attributes 
+    """
+    pass
+
+@click.command("general")
+@click.option('--path', default=DEFAULT_DB_PATH, help="Path to sqlite DB file")
+def info_general(path):
+    """Show general aggregate information"""
+    sqlIrSystem = connect(path)
+    result = sqlIrSystem.info()
+    max_key_len = max([len(str(r)) for r in result.keys()])
+    max_value_len = max([len(format(v, "0,d")) for v in result.values()])
+    for k,v in result.items():
+        print("%s: %s" % (k.rjust(max_key_len + 2), format(v, "%s,d" % (max_value_len + 2))))
+
+@click.command("pieces")
+@click.option('--path', default=DEFAULT_DB_PATH, help="Path to sqlite DB file")
+@click.option('--name', default="", help="Case-insensitive plain text filter to match against name")
+@click.option('--fname', default="", help="Case-insensitive plain text filter to match against file path")
+def info_pieces(path, name, fname):
+    """List pieces and composers"""
+    sqlIrSystem = connect(path)
+    result = filter(
+                lambda x: fname.lower() in x[1].lower(),
+                filter(lambda x: name.lower() in x[0].lower(),
+                    sqlIrSystem.pieces()))
+    print(list(result))
 
 def print_results(grader_results):
     table_rows = []
@@ -91,9 +123,13 @@ def print_results(grader_results):
 def cli():
     pass
 
+info.add_command(info_pieces)
+info.add_command(info_general)
+
 cli.add_command(create)
 cli.add_command(add_piece)
 cli.add_command(query_tiny)
+cli.add_command(info)
 
 if __name__ == "__main__":
     cli()
