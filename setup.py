f975c3cbd8d808ad8a786b1a134a987447ef9009
Devon Terrell <axiomabsolute@gmail.com>
2017-12-19 14:46:32 -0500

    Augment setup

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
-setup(name='firms',
+setup(
+    name='firms',
     version='0.0.1',
     description='Fuzzy Information Retrieval for Musical Scores',
     url='https://github.com/axiomabsolute/cs410-information-retrieval',
+    download_url='https://github.com/axiomabsolute/cs410-information-retrieval/archive/0.0.1.tar.gz',
     author='Devon Terrell',
     author_email='devon@axioms.me',
     license='MIT',
     packages=['firms'],
-    python_requires=">=3"
+    python_requires=">=3",
     install_requirements=[
         'music21',
         'tabulate',
