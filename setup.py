1ff39aa3770ad1bd4af70de2f23c2cda588462ec
Devon Terrell <axiomabsolute@gmail.com>
2017-12-19 14:44:35 -0500

    Set version number and python requires

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(name='firms',
-    version='0.0.0',
+    version='0.0.1',
     description='Fuzzy Information Retrieval for Musical Scores',
     url='https://github.com/axiomabsolute/cs410-information-retrieval',
     author='Devon Terrell',
     author_email='devon@axioms.me',
     license='MIT',
     packages=['firms'],
+    python_requires=">=3"
     install_requirements=[
         'music21',
         'tabulate',
