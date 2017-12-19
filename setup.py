8bb347e8b390864393bd750711aaaa8652c3b3b5
Devon Terrell <axiomabsolute@gmail.com>
2017-12-19 15:25:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='firms',
-    version='0.0.2',
+    version='0.0.4',
     description='Fuzzy Information Retrieval for Musical Scores',
     url='https://github.com/axiomabsolute/cs410-information-retrieval',
-    download_url='https://github.com/axiomabsolute/cs410-information-retrieval/archive/0.0.2.tar.gz',
+    download_url='https://github.com/axiomabsolute/cs410-information-retrieval/archive/0.0.4.tar.gz',
     author='Devon Terrell',
     author_email='devon@axioms.me',
     license='MIT',
