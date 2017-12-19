d8053e5b77ce403582b185a98b474de65c9c9500
Devon Terrell <axiomabsolute@gmail.com>
2017-12-19 09:02:03 -0500

    Clean up old sql scripts

--- sql_scripts/entryless-pieces.sql
+++
@@ -1,6 +0,0 @@
-SELECT pieces.name, parts.name, snippets.offset, stems.stem, entries.id FROM snippets
-LEFT JOIN pieces ON snippets.piece_id=pieces.id
-LEFT JOIN parts ON snippets.part_id=parts.id
-LEFT JOIN entries ON snippets.id=entries.snippet_id
-LEFT JOIN stems ON entries.stem_id = stems.id
-WHERE entries.id IS NULL
