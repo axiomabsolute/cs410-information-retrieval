6eb54606b26001b7e0f57793f22a365f4caeb9de
Devon Terrell <axiomabsolute@gmail.com>
2017-12-19 20:17:24 -0500

    Remove readme.md, use only rst. Update setup.

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='firms',
-    version='0.0.8',
+    version='0.0.9',
     description='Fuzzy Information Retrieval for Musical Scores',
+    long_description=long_description,
     url='https://github.com/axiomabsolute/cs410-information-retrieval',
-    download_url='https://github.com/axiomabsolute/cs410-information-retrieval/archive/0.0.8.tar.gz',
+    download_url='https://github.com/axiomabsolute/cs410-information-retrieval/archive/0.0.9.tar.gz',
     author='Devon Terrell',
     author_email='devon@axioms.me',
     license='MIT',
